83887a5f14be23bf60645c1166f9d55ecbf69828
Ben Visser <theodore.r.visser@gmail.com>
2015-07-05 18:03:40 -0700

    updated client example

--- client_example.rb
+++ client_example.rb
@@ -1,12 +1,12 @@
 require 'unirest'
 
 
-response = Unirest.post "http://localhost:3000/convert",
+response = Unirest.post "http://127.0.0.1:3000/convert",
   parameters:{
-    "file" => File.new("gotham.ttf"),
+    "file" => File.new(ARGV[0]),
     "format" => "woff",
   }
 
-open('gotham.woff', 'wb') do |file|
+open('font.woff', 'wb') do |file|
   file.write(response.body);
 end
